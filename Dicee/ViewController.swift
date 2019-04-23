1ad644e84a754d3616c5097a6f3e52440b0d3acf
imac1 <Februarie1977>
2019-04-23 08:21:34 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by MacBook Air on 23/04/2019.
+//  Copyright © 2019 MacBook Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
